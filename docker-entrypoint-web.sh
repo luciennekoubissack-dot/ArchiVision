8670de2f7c5bdbb09e4fe4df84996b9903928c0a
luciennekoubissack-dot <luciennekoubissack@gmail.com>
2026-09-06 15:32:26 +0100

    nginx

--- docker-entrypoint-web.sh
+++ docker-entrypoint-web.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
-# Génère la config Nginx en remplaçant NGINX_API_URL par sed (pas envsubst)
-# pour ne pas toucher aux variables internes Nginx ($host, $remote_addr, etc.)
+# Génère la config Nginx en remplaçant le placeholder par l'URL de l'API.
+# Utilise la syntaxe sed POSIX compatible avec busybox (Alpine).
 
 set -e
 
 : "${NGINX_API_URL:=http://api:3000}"
 
-sed "s|NGINX_API_URL_PLACEHOLDER|${NGINX_API_URL}|g" \
+# Remplace le placeholder par l'URL de l'API (syntaxe sed busybox/Alpine)
+sed "s#NGINX_API_URL_PLACEHOLDER#${NGINX_API_URL}#g" \
   /etc/nginx/templates/archivision.conf.template \
   > /etc/nginx/conf.d/archivision.conf
 
